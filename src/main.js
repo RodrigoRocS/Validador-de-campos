2efbf90830a61e2f5557cd0cc7327ea17eba8e7a
RodrigoRocS <rrsilva1701@gmail.com>
2023-02-08 20:33:34 -0300

    exercicio 1.1

--- src/main.js
+++ src/main.js
@@ -1,1 +1,21 @@
 import validator from 'validator';
+
+import './style.css';
+
+const getBtn = document.querySelector('button');
+const getInput = document.querySelector('#caixa');
+const getSelect = document.querySelector('#select')
+const getParagraph = document.querySelector('p');
+
+
+getBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+
+
+  const objetaoDeFuncao = {
+    cpf: validator.isTaxID(getInput.value, 'pt-BR'),
+    email: validator.isEmail(getInput.value),
+    hexColor: validator.isHexColor(getInput.value),
+  }
+  getParagraph.innerHTML = `A validação retornou ${objetaoDeFuncao[getSelect.value]}`
+})
